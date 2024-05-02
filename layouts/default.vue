e32bd4b8e361f721d19758947af59dd522a67df9
n1k_02 <ghostwarriorn@gmail.com>
2024-05-02 18:01:31 +0500

    rem: 10px-18px

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,6 +18,7 @@
     min-width: 700px;
     container: layout / inline-size;
     background: $grey;
+    overflow: auto;
 }
 </style>
 
@@ -29,5 +30,8 @@
     flex-direction: column;
     flex: 0 0 20rem;
     padding: px-to-vw(48px) px-to-vw(32px) ; //48px 32px 
+}
+.slot {
+    flex: 0 0 80rem;
 }
 </style>
