9bfafc394967f0e2cedde190a762462bcd1f0708
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-05-03 21:57:00 +0500

    input component

--- components/UI/Inputs/input.vue
+++ components/UI/Inputs/input.vue
@@ -1,58 +1,95 @@
 <template>
   <div class="input-field">
-    <input required="" name="text" type="text"/>
-    <label>Enter your email</label>
+    <input required name="text" :type="type" v-model="input"/>
+    <label>{{ placeholder }}</label>
+    <button @click="clearInput" v-if="input !== ''">
+      <nuxt-icon name="Close" filled/>
+    </button>
   </div>
 </template>
 
 <script setup>
-const input = null;
 
+import Button from "~/components/UI/Buttons/Button/button.vue";
+
+const props = defineProps({
+  placeholder: String,
+  type: String
+});
+
+const input = ref('');
+
+const clearInput = () => {
+  input.value = '';
+}
 </script>
+
 
 <style lang="scss" scoped>
 .input-field {
   position: relative;
   color: $blue-grey;
-}
 
-.input-field label {
-  position: absolute;
-  top: 50%;
-  left: 16px;
-  transform: translateY(-50%);
-  font-size: 16px;
-  pointer-events: none;
-  transition: 0.15s ease;
-}
 
-.input-field input {
-  width: calc(100% - 32px); //  width - padding-inline
-  height: 64px;
-  background: transparent;
-  background: $light-grey;
-  border-radius: 12px;
-  border: none;
-  outline: none;
-  font-size: 16px;
-  padding-inline: 16px;
-  font-weight: $fw-semibold;
-
-  &:hover {
-    outline: 1px $main-dark solid;
+  label {
+    position: absolute;
+    top: 50%;
+    left: 16px;
+    transform: translateY(-50%);
+    font-size: $font-md;
+    pointer-events: none;
+    transition: 0.15s ease;
   }
 
-  &:focus {
-    background: $white;
-    color: $main-dark;
-    outline: 1px $main-dark solid;
+  input {
+    width: calc(100% - 32px - 30px); // ширина - отступ справа для кнопки
+    height: calc(64px - 15px);
+    background: transparent;
+    background: $light-grey;
+    border-radius: 12px;
+    border: none;
+    outline: none;
+    font-size: $font-md;
+    padding-left: 16px;
+    padding-right: 30px;
+    padding-top: 15px;
+    font-weight: $fw-semibold;
+
+    &:hover {
+      outline: 1px $main-dark solid;
+    }
+
+    &:focus {
+      background: $white;
+      color: $main-dark;
+      outline: 1px $main-dark solid;
+    }
+  }
+
+  button {
+    display: none;
+    position: absolute;
+    top: 50%;
+    right: 24px;
+    width: 24px;
+    height: 24px;
+    transform: translateY(-50%);
+    background: transparent;
+    cursor: pointer;
+    outline: none;
   }
 }
 
 .input-field input:focus ~ label,
 .input-field input:valid ~ label {
   font-size: $font-xsm;
-  top: 12px;
+  top: 20px;
   left: 16px;
+}
+
+.input-field:focus-within button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
