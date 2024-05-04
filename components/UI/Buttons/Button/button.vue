87d8a6b0149727a26ee192b6818b2bee495f8fd9
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-05-04 17:08:11 +0500

    button component

--- components/UI/Buttons/Button/button.vue
+++ components/UI/Buttons/Button/button.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style lang="scss" scoped>
+$button-hover: #0D1543;
+$button-active: #0C112E;
 .button {
   height: 56px;
   border-radius: 12px;
@@ -13,5 +15,18 @@
   background: $main-dark;
   border: none;
   cursor: pointer;
+
+  &:hover {
+    background: $button-hover;
+  }
+
+  &:active {
+    background: $button-active;
+  }
+
+  &:disabled {
+    background: $grey;
+    color: $dark-grey;
+  }
 }
 </style>
