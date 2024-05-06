863d8cd958eefd5fcfdfab09d59abd82234ccf0f
n1k_02 <ghostwarriorn@gmail.com>
2024-05-06 21:54:10 +0500

    +card +main-layouts-list +main-mailing-list

---
+++ components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card">
+        <div class="card__header" v-if="$slots.header">
+            <slot name="header" />
+        </div>
+        <div class="card__main" v-if="$slots.main">
+            <slot name="main" />
+        </div>
+        <div class="card__footer" v-if="$slots.footer">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+$border: 1px solid $grey;
+
+.card {
+    background: $white;
+    border-radius: 1.25rem;
+    padding: 2rem;
+
+    &__header {}
+
+    &__main {
+        margin-top: 2rem;
+        padding: 2rem 0;
+        border-top: $border;
+    }
+
+    &__footer {
+        padding-top: 2rem;
+        border-top: $border;
+    }
+}
+</style>
---
+++ components/GraphicalCharts.vue
@@ -0,0 +1,18 @@
+<template>
+    <card>
+        <template #header>
+            <h1>Графические отчёты</h1>
+        </template>
+        <template #main>
+            <div>filter</div>
+            <div>charts</div>
+
+        </template>
+    </card>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
