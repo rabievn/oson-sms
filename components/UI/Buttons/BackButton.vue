0089f249c60997e48022087f10d7da5248fef6fd
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-05-18 10:12:37 +0500

    hover & active for backbutton

--- components/UI/Buttons/BackButton.vue
+++ components/UI/Buttons/BackButton.vue
@@ -1,12 +1,10 @@
 <template>
   <button @click="handleClick" class="back-button">
-    <nuxt-icon class="back-button__icon" name="reused/ArrowLeft" filled />
+    <nuxt-icon class="back-button__icon" name="reused/ArrowLeft" filled/>
   </button>
 </template>
 
 <script setup>
-import { defineEmits } from 'vue'
-
 const emit = defineEmits(['backClick'])
 
 const handleClick = () => {
@@ -17,23 +15,31 @@ const handleClick = () => {
 
 <style lang="scss">
 .back-button {
-    background-color: $main-dark;
-    width: 2.5rem;
-    height: 2.5rem;
-    border-radius: 0.5rem;
-    border: 2px solid $grey;
-    @include flexCenter;
+  background-color: $main-dark;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 0.5rem;
+  border: 2px solid $grey;
+  @include flexCenter;
 
-    &__icon {
-        width: 1.5rem;
-        height: 1.5rem;
+  &:hover {
+    background: $secondary-dark-blue;
+  }
 
-        svg,
-        path {
-            width: 100%;
-            height: 100%;
-            stroke: $white;
-        }
+  &:active {
+    background: $brown-black;
+  }
+
+  &__icon {
+    width: 1.5rem;
+    height: 1.5rem;
+
+    svg,
+    path {
+      width: 100%;
+      height: 100%;
+      stroke: $white;
     }
+  }
 }
 </style>
