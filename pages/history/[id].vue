3412264a930d80ac79b358dfeb5c842562b149c5
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-06-05 10:20:26 +0500

    input added for history id

--- pages/history/[id].vue
+++ pages/history/[id].vue
@@ -9,8 +9,23 @@
         </div>
       </template>
       <template #main>
-
-        123
+        <form class="changeForm">
+          <div class="changeForm__row">
+            <Input placeholder="Группа" v-model="formData.group"/>
+            <Input placeholder="SMS отправитель" v-model="formData.smsSender"/>
+          </div>
+          <div class="changeForm__row">
+            <Input placeholder="Дата создания" v-model="formData.creationDate"/>
+            <Input placeholder="Дата завершения" v-model="formData.completionDate"/>
+          </div>
+          <div class="changeForm__row">
+            <Input placeholder="Количество получателей" v-model="formData.recipients"/>
+            <Input placeholder="SMS отправитель" v-model="formData.secondSmsSender"/>
+          </div>
+          <div class="changeForm__row">
+            <Input placeholder="Сумма" v-model="formData.amount"/>
+          </div>
+        </form>
       </template>
     </card>
     <card>
@@ -28,6 +43,7 @@
 import BackButtonWithText from "~/components/UI/Buttons/BackButtonWithText.vue";
 import WithAsideLayout from "~/layouts/withAsideLayout.vue";
 import PieChart from "~/components/charts/PieChart.vue";
+import Input from "~/components/UI/Inputs/Input.vue";
 
 const statusChartData = [
   {
@@ -47,8 +63,38 @@ const statusChartData = [
   }
 ];
 
+const formData = ref({
+  group: 'Шаходатнома махсус',
+  smsSender: 'RASIN SUGHD',
+  creationDate: '13.01.2023 / 14:00:23',
+  completionDate: '13.01.2023 / 14:00:23',
+  recipients: '425',
+  secondSmsSender: 'RASIN SUGHD',
+  amount: '25,55 с.'
+});
 </script>
 
 <style lang="scss" scoped>
+.changeForm {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 
+  &__row {
+    display: flex;
+    width: 100%;
+    gap: 1.5rem;
+
+    & > * {
+      flex: 1 1 50%;
+    }
+  }
+
+
+  &__row:last-child {
+    & > * {
+      flex: 0 0 calc(50% - 0.75rem);
+    }
+  }
+}
 </style>
